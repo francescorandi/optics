c6b9dba27cb00e827a82df12dbdb9d46b878d38f
Francesco Randi <francesco.randi@studenti.units.it>
2016-03-05 18:41:34 +0100

    List of available lineshapes

--- Oscillators/__init__.py
+++ Oscillators/__init__.py
@@ -3,4 +3,7 @@ from Oscillators.Drude import Drude
 from Oscillators.Lorentz import Lorentz
 from Oscillators.Gauss import Gauss
 
+def list():
+    print("Available lineshapes: Drude, Lorentz, Gauss")
+
 __all__ = ["Drude", "Lorentz", "Gauss"]
