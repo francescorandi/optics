e7eef0605d972e216b3d9755cd3407047c202d56
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2016-06-20 17:41:56 +0200

    Skeleton for setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='PyOpST',
+        version='0.1',
+        packages=['Oscillators'],
+        url='https://github.com/francescorandi/optics',
+        license='',
+        author='Ignacio Vergara / Francesco Randi',
+        author_email='',
+        description='A toolbox for Optical Spectroscopy data analysis in the context of condensed matter physics.'
+)
